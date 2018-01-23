eeda4bca4be1cf5ca1832a21c46f72f4241ad522
Jacob Carlsson <jacob.carlsson@gmail.com>
2018-01-23 20:22:56 +0100

    Update visualize

--- python-visualize/visualize_file.py
+++ python-visualize/visualize_file.py
@@ -17,6 +17,7 @@ def read_file(filepath):
     """Read CSV filepath and return parsed values."""
     watts_list = []
     datetime_list = []
+    total_watt_hours = 0
 
     with open(filepath, newline='') as f:
         reader = csv.DictReader(f, delimiter=';')
@@ -27,14 +28,26 @@ def read_file(filepath):
             watts = float(row['watts'])
             datetime_list.append(_datetime)
             watts_list.append(watts)
+            total_watt_hours += 1
+    print('Total consumption: {:.2f} kWh'.format(total_watt_hours / 1000))
     return (datetime_list, watts_list)
+
+def moving_average(data, num_points=3):
+    new_data = []
+
+    for i, value in enumerate(data[:-num_points]):
+        new_data.append(sum(data[i:i+num_points]) / num_points)
+    return new_data
+
+
 
 def show_plot(datetime_list, watts_list):
     """Show a plot of the data."""
     # Configure dates on X-axis
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 
-    plt.plot(datetime_list, watts_list)
+    plt.plot(datetime_list, watts_list, 'b-x')
+    # plt.plot(datetime_list[:-5], moving_average(watts_list, 5), color='red')
     plt.ylabel('Watts')
     plt.xlabel('Local time')
     plt.title('Measurements between {} and {}'.format(datetime_list[0].strftime('%Y-%m-%d %H:%M:%S'),
